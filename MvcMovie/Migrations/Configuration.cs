c2895ccfdeb5ebab8c2a88784ce8b5235fb2c9f8
AHBICJ <810988732@qq.com>
2019-09-17 21:32:00 +0800

    Adding a Rating Property to the Movie Model

--- MvcMovie/Migrations/Configuration.cs
+++ MvcMovie/Migrations/Configuration.cs
@@ -25,18 +25,21 @@ namespace MvcMovie.Migrations
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-1-11"),
                     Genre = "Romantic Comedy",
+                    Rating = "PG",
                     Price = 7.99M
                 }, new Movie
                 {
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
+                    Rating = "PG",
                     Price = 8.99M
                 }, new Movie
                 {
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
+                    Rating = "PG",
                     Price = 9.99M
                 }, new Movie
                 {
